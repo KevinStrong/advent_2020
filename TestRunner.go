e6ca72ce5311f12f735dc09225ca9ccd6e870949
KevinColbyStrong <Kevin.colby.strong@gmail.com>
2020-12-14 02:30:26 -0800

    Solve day 14 part 2

--- TestRunner.go
+++ TestRunner.go
@@ -1,39 +1,13 @@
 package main
 
 import (
-	"advent_2020/day1"
-	"advent_2020/day10"
-	"advent_2020/day11"
-	"advent_2020/day12"
-	"advent_2020/day13"
 	"advent_2020/day14"
-	"advent_2020/day2"
-	"advent_2020/day3"
-	"advent_2020/day4"
-	"advent_2020/day5"
-	"advent_2020/day6"
-	"advent_2020/day7"
-	"advent_2020/day8"
-	"advent_2020/day9"
 	"fmt"
 	"time"
 )
 
 func main() {
 	start := time.Now()
-	fmt.Println(day1.Solve(ReadLinesNumbers("day1/input.txt")))
-	fmt.Println(day2.Solve(ReadLines("Day2/input.txt")))
-	fmt.Println(day3.Solve(ReadLines("Day3/input.txt")))
-	fmt.Println(day4.Solve(ReadLines("Day4/input.txt")))
-	fmt.Println(day5.Solve(ReadLines("Day5/input.txt")))
-	fmt.Println(day6.Solve(ReadLines("Day6/input.txt")))
-	fmt.Println(day7.Solve(ReadLines("Day7/input.txt")))
-	fmt.Println(day8.Solve(ReadLines("Day8/input.txt")))
-	fmt.Println(day9.Solve(ReadLinesNumbers("Day9/input.txt")))
-	fmt.Println(day10.Solve(ReadLinesNumbers("Day10/input.txt")))
-	fmt.Println(day11.Solve(ReadLines("Day11/input.txt")))
-	fmt.Println(day12.Solve(ReadLines("Day12/input.txt")))
-	fmt.Println(day13.Solve(ReadLines("Day13/input.txt")))
 	fmt.Println(day14.Solve(ReadLines("Day14/input.txt")))
 	fmt.Printf("Execution took %s", time.Since(start))
 }
--- day14/day14.go
+++ day14/day14.go
@@ -1,45 +1,56 @@
 package day14
 
 import (
+	"math"
+	"math/bits"
 	"regexp"
 	"strconv"
 	"strings"
 )
 
 type Mask struct {
+	maskXs       []Wildcard
 	maskOnes     uint64
-	maskZeros    uint64
 	instructions []Instruction
 }
 
 type Instruction struct {
-	value    uint64
-	location int
+	value    int
+	location uint64
+}
+
+type Wildcard struct {
+	setOnes  uint64
+	setZeros uint64
 }
 
 func Solve(input []string) int {
-	return solvePartOne(input)
+	return solvePartTwo(input)
 }
 
-func solvePartOne(input []string) int {
+// See previous commit for solvePartOne
+func solvePartTwo(input []string) int {
 	allMasks := createInstructions(input)
 	results := processInstructions(allMasks)
-	return int(sumValues(results))
+	return sumValues(results)
 }
 
-func sumValues(results map[int]uint64) uint64 {
-	var total uint64 = 0
+func sumValues(results map[uint64]int) int {
+	var total = 0
 	for memLocation := range results {
 		total += results[memLocation]
 	}
 	return total
 }
 
-func processInstructions(masks []Mask) map[int]uint64 {
-	results := make(map[int]uint64)
+func processInstructions(masks []Mask) map[uint64]int {
+	results := make(map[uint64]int)
 	for _, mask := range masks {
 		for _, instruction := range mask.instructions {
-			results[instruction.location] = (mask.maskOnes | instruction.value) & mask.maskZeros
+			for _, wildcardMask := range mask.maskXs {
+				location := (instruction.location | mask.maskOnes | wildcardMask.setOnes) & wildcardMask.setZeros
+				results[location] = instruction.value
+			}
 		}
 	}
 	return results
@@ -48,8 +59,8 @@ func processInstructions(masks []Mask) map[int]uint64 {
 func createInstructions(input []string) []Mask {
 	var masks = make([]Mask, 0)
 	var currentMask = Mask{
+		maskXs:       make([]Wildcard, 0),
 		maskOnes:     0,
-		maskZeros:    0,
 		instructions: make([]Instruction, 0),
 	}
 	for _, inputLine := range input {
@@ -70,8 +81,8 @@ func createInstructions(input []string) []Mask {
 func buildMemoryInstructionInstruction(line string) Instruction {
 	var maskValue = regexp.MustCompile(`^mem\[(\d+)] = (\d+)$`)
 	captureGroups := maskValue.FindStringSubmatch(line)
-	location, _ := strconv.Atoi(captureGroups[1])
-	value, _ := strconv.ParseUint(captureGroups[2], 10, 36)
+	location, _ := strconv.ParseUint(captureGroups[1], 10, 64)
+	value, _ := strconv.Atoi(captureGroups[2])
 	return Instruction{
 		value:    value,
 		location: location,
@@ -82,12 +93,46 @@ func createMask(line string) Mask {
 	var maskValue = regexp.MustCompile(`^mask = (.*)$`)
 	captureGroups := maskValue.FindStringSubmatch(line)
 	replaceXWithZeros := strings.ReplaceAll(captureGroups[1], "X", "0")
-	replaceXWithOnes := strings.ReplaceAll(captureGroups[1], "X", "1")
+
+	replaceOnesWithZeros := strings.ReplaceAll(captureGroups[1], "1", "0")
+	thenConvertXsToOnes := strings.ReplaceAll(replaceOnesWithZeros, "X", "1")
+
+	maskXs, _ := strconv.ParseUint(thenConvertXsToOnes, 2, 64)
 	maskOnes, _ := strconv.ParseUint(replaceXWithZeros, 2, 64)
-	maskZeros, _ := strconv.ParseUint(replaceXWithOnes, 2, 64)
 	return Mask{
+		maskXs:       buildWildcards(maskXs),
 		maskOnes:     maskOnes,
-		maskZeros:    maskZeros,
 		instructions: make([]Instruction, 0),
 	}
+}
+
+func buildWildcards(xs uint64) []Wildcard {
+	var allInstructionLocations = make([]Wildcard, 0)
+	allInstructionLocations = append(allInstructionLocations, Wildcard{
+		setOnes:  0,
+		setZeros: math.MaxUint64,
+	})
+	for i := 0; i < 64; i++ {
+		// If the first bit is a 1 then it is a wildcard
+		if 1&xs == 1 {
+			var tempInstructions = make([]Wildcard, 0)
+			for _, loc := range allInstructionLocations {
+				tempInstructions = append(tempInstructions, Wildcard{
+					setOnes:  loc.setOnes | 1,
+					setZeros: loc.setZeros,
+				})
+				tempInstructions = append(tempInstructions, Wildcard{
+					setOnes:  loc.setOnes,
+					setZeros: loc.setZeros & (math.MaxUint64 - 1), // All 1s except first bit
+				})
+			}
+			allInstructionLocations = tempInstructions
+		}
+		xs = bits.RotateLeft64(xs, 1)
+		for i := range allInstructionLocations {
+			allInstructionLocations[i].setOnes = bits.RotateLeft64(allInstructionLocations[i].setOnes, 1)
+			allInstructionLocations[i].setZeros = bits.RotateLeft64(allInstructionLocations[i].setZeros, 1)
+		}
+	}
+	return allInstructionLocations
 }
